82de6e549991ecb8dd114c97b2cefc502597dfae
Siddhartha <biz.siddhartha@gmail.com>
2024-08-19 17:26:30 +0545

    add dialog edit profile

--- HRMS-Project/src/app/features/private/profile-details/profile-details.module.ts
+++ HRMS-Project/src/app/features/private/profile-details/profile-details.module.ts
@@ -4,15 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ProfileDetailsRoutingModule } from './profile-details-routing.module';
 import { ProfileDetailsComponent } from './profile-details.component';
 import { MaterialsModule } from '../../../materials/materials.module';
-import { MatButtonModule } from '@angular/material/button';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 
 @NgModule({
-  declarations: [ProfileDetailsComponent],
+  declarations: [
+    ProfileDetailsComponent,
+    ProfileEditComponent
+  ],
   imports: [
     CommonModule,
-    ProfileDetailsRoutingModule,
-    MaterialsModule,
-    MatButtonModule,
-  ],
+    ProfileDetailsRoutingModule, MaterialsModule,
+  ]
 })
-export class ProfileDetailsModule {}
+export class ProfileDetailsModule { }
--- HRMS-Project/src/app/materials/materials.module.ts
+++ HRMS-Project/src/app/materials/materials.module.ts
@@ -2,12 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-
+import { MatButtonModule, } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogActions, MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatGridListModule, MatButtonModule],
-  exports: [MatGridListModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+  ],
+  exports: [
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule,
+    MatDialogActions,
+  ],
 })
 export class MaterialsModule {}
