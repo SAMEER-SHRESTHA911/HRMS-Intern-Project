3cbe48ebaa52a7403fa72a3e2fd97b248b12a283
Siddhartha <biz.siddhartha@gmail.com>
2024-08-20 17:17:06 +0545

    added dashboard

--- HRMS-Project/src/app/features/private/private-routing.module.ts
+++ HRMS-Project/src/app/features/private/private-routing.module.ts
@@ -21,6 +21,11 @@ const routes: Routes = [
         (m) => m.StaffRegistrationModule
       ),
   },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
 ];
 
 @NgModule({
