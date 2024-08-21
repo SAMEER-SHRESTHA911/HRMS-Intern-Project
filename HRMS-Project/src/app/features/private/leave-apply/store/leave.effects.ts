bef61d81b1b225f2c9e82a91146d0b72536c2774
Sameer <samirstha2002@gmail.com>
2024-08-21 16:50:11 +0545

    Added a feature in leave apply

---
+++ HRMS-Project/src/app/features/private/leave-apply/store/leave.effects.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { LeaveApplyBody, LeaveApplyResponse } from '../types/types';
+import {
+  leaveApply,
+  submitLeaveForm,
+  submitLeaveFormFail,
+  submitLeaveFormSuccess,
+} from './leave.actions';
+import { catchError, map, mergeMap, of, switchMap } from 'rxjs';
+import { LeaveApplyApiService } from '../services/api/leave-api.service';
+
+@Injectable()
+export class LeaveEffects {
+  constructor(
+    private action$: Actions,
+    private leaveService: LeaveApplyApiService
+  ) {}
+
+  submitLeaveForm$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(submitLeaveForm),
+      switchMap((action) =>
+        this.leaveService.addLeaveRequest(action.leaveData).pipe(
+          map((leaveData: LeaveApplyBody) =>
+            submitLeaveFormSuccess({ leaveData })
+          ),
+          catchError((error) => of(submitLeaveFormFail({ error })))
+        )
+      )
+    )
+  );
+}
---
+++ HRMS-Project/src/app/features/private/leave-apply/types/types.ts
@@ -0,0 +1,19 @@
+export interface LeaveApplyBody {
+  employeeId: number;
+  leaveFrom: string;
+  leaveTo: string;
+  leaveType: number;
+  dayLeave: number;
+  leaveRequestStatus: number;
+  reasonForLeave: string;
+}
+
+export interface LeaveApplyResponse{
+    result: number,
+    message: string,
+    data: boolean
+}
+export interface DayLeave {
+  id: number;
+  name:string;
+}
