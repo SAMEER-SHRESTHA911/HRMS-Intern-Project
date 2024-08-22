e83c68df9c919d4dc0e4587b7eb4275b7d8c42bf
Siddhartha <biz.siddhartha@gmail.com>
2024-08-22 18:00:04 +0545

    profile and checkin out

--- HRMS-Project/src/app/app.module.ts
+++ HRMS-Project/src/app/app.module.ts
@@ -7,16 +7,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { provideHttpClient } from '@angular/common/http';
-
+import { DialogComponent } from './shared/components/dialog/dialog.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
-
   ],
   providers: [provideAnimationsAsync(), provideHttpClient()],
   bootstrap: [AppComponent],
--- HRMS-Project/src/app/features/private/dashboard/dashboard.module.ts
+++ HRMS-Project/src/app/features/private/dashboard/dashboard.module.ts
@@ -4,10 +4,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { MatCardModule } from '@angular/material/card';
+import { MaterialsModule } from '../../../materials/materials.module';
 
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, MatCardModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatCardModule,
+    MaterialsModule,
+  ],
 })
 export class DashboardModule {}
