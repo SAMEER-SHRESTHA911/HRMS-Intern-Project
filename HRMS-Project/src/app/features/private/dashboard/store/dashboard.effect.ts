2f870c6b1d74ff026583a0074f316943f19f480a
Siddhartha <biz.siddhartha@gmail.com>
2024-08-23 21:15:50 +0545

    added store to dashboard

---
+++ HRMS-Project/src/app/features/private/dashboard/store/dashboard.effect.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import {
+  loadDashboard,
+  loadDashboardFailure,
+  loadDashboardSuccess,
+} from './dashboard.actions';
+import { catchError, map, mergeMap, of } from 'rxjs';
+import { DashboardService } from '../services/dashboard.service';
+@Injectable()
+export class DashboardEffect {
+  loadStaff$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadDashboard),
+      mergeMap(() =>
+        this.dashboardService.getDashbaordData().pipe(
+          map((dashboard) => loadDashboardSuccess({ dashboard })),
+          catchError((error) =>
+            of(loadDashboardFailure({ error: error.message }))
+          )
+        )
+      )
+    )
+  );
+  constructor(
+    private actions$: Actions,
+    private dashboardService: DashboardService
+  ) {}
+}
