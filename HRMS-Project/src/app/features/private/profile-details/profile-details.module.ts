9d731f02e052f7e8f10534a222e00251324ca42b
Siddhartha <biz.siddhartha@gmail.com>
2024-08-26 18:12:17 +0545

    resolved conflicts on dashboard

--- HRMS-Project/src/app/features/private/profile-details/profile-details.module.ts
+++ HRMS-Project/src/app/features/private/profile-details/profile-details.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileDetailsRoutingModule } from './profile-details-routing.module';
-import { ProfileDetailsComponent } from './profile-details.component';
 import { MaterialsModule } from '../../../materials/materials.module';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileDetailsComponent } from './profile-details.component';
 
 @NgModule({
   declarations: [ProfileDetailsComponent, ProfileEditComponent],
