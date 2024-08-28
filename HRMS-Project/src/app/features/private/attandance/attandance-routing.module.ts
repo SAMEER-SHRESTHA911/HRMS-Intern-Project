08e69bee33db1dc69467e95fa8c390a16567efcd
abhisha <abhishasharma004@gmail.com>
2024-08-28 13:32:53 +0545

    make some changes on the routing part

--- HRMS-Project/src/app/features/private/attandance/attandance-routing.module.ts
+++ HRMS-Project/src/app/features/private/attandance/attandance-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AttandanceDetailsComponent } from './attandance-details/attandance-details.component';
 import { CommonModule } from '@angular/common';
-import { AttandanceCalanderComponent } from './attandance-calander/attandance-calander.component';
 
 const routes: Routes = [
   {
