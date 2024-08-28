dc5949c092ede435a15d350ef2bea16eda573832
Sameer <samirstha2002@gmail.com>
2024-08-28 14:17:00 +0545

    Addded some functionality

--- HRMS-Project/src/app/features/private/leave-management/components/leave-approve-reject/leave-approve-reject.component.ts
+++ HRMS-Project/src/app/features/private/leave-management/components/leave-approve-reject/leave-approve-reject.component.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { LeaveConfirmationService } from '../../services/leave-confirmation/leave-confirmation.service';
+import { LeaveRequestList } from '../../types/leave-table';
+import { Store } from '@ngrx/store';
+import { selectLeaveRequestList } from '../../store/leave-confirmation/leave-confirmation.selector';
+import { Observable, of } from 'rxjs';
+import { FETCH_LEAVE_REQUEST_lIST } from '../../store/leave-confirmation/leave-confirmation.actions';
 
 @Component({
   selector: 'app-leave-approve-reject',
@@ -8,10 +13,19 @@ import { LeaveConfirmationService } from '../../services/leave-confirmation/leav
   styleUrl: './leave-approve-reject.component.scss'
 })
 export class LeaveApproveRejectComponent implements OnInit{
-  constructor(private  http: HttpClient,private leaveRequestList: LeaveConfirmationService) { }
+  constructor(private  http: HttpClient, private leaveRequestList: LeaveConfirmationService, private store: Store) { }
   
+  requestList$ : Observable<LeaveRequestList[]> = of([]);
+
   ngOnInit(): void {
-      this.leaveRequestList.fetchRequestList().subscribe((data)=> console.log(data))
+      this.initializeStore()
+  }
+
+  initializeStore(){
+    this.store.dispatch(FETCH_LEAVE_REQUEST_lIST());
+     this.store.select(selectLeaveRequestList).subscribe(
+      data => console.log(data)
+     )
   }
   
 }
