291e451f476f7d1b0ad580573921b7eb459483f8
Siddhartha <biz.siddhartha@gmail.com>
2024-09-09 09:05:44 +0545

    added calendar

--- HRMS-Project/src/app/features/private/dashboard/dashboard.component.ts
+++ HRMS-Project/src/app/features/private/dashboard/dashboard.component.ts
@@ -16,6 +16,9 @@ export class DashboardComponent implements OnInit {
   loading$: Observable<boolean> = of(false);
   error$: Observable<string | null> = of(null);
   today = DAY_DATA;
+  userRole: string | null = localStorage.getItem('role');
+  //"Admin" , "Employee"
+
   constructor(private store: Store<DashboardState>) {}
 
   selectorInitilizer(): void {}
--- HRMS-Project/src/app/shared/constants/global.constants.ts
+++ HRMS-Project/src/app/shared/constants/global.constants.ts
@@ -1,8 +1,8 @@
 // export const baseUrl = 'http://localhost:3000';
 // export const baseUrl = 'https://zg0qm2qz-1595.inc1.devtunnels.ms/apigateway';
-// export const baseUrl = 'http://192.168.1.21:5000/apigateway';
+export const baseUrl = 'http://192.168.1.21:5000/apigateway';
 
-export const baseUrl = `http://localhost:5262/apigateway`;
+// export const baseUrl = `http://localhost:5262/apigateway`;
 // export const baseUrl = `http://localhost:1595/apigateway`;
 
 interface ILoggedInUser {
